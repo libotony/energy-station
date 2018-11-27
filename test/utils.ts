d9b38b4032341290079dcd679acb5709f54811d0
tony <tony.li@vechain.com>
2018-11-27 19:49:06 +0800

    test util: make assert fail aceept revert and throw

--- test/utils.ts
+++ test/utils.ts
@@ -5,7 +5,7 @@ export const AssertFail = async function (asyncFn: Function) {
         await asyncFn
         throw new Error('Expected revert not received')
     } catch (e) {
-        const RevertRegex = new RegExp(/Transaction:[\s\S]*exited\ with\ an\ error\ \(status\ 0\)\ after\ consuming\ all\ gas/)
+        const RevertRegex = new RegExp(/Transaction:[\s\S]*exited\ with\ an\ error\ \(status\ 0\)/)
         expect(RevertRegex.test(e.message)).to.be.equal(true, `Expected "status 0(revert)", got ${e.message} instead`)
     }
 }
