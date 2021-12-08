a5372898943a6303bfc535b3f927170b01f62f9c
libotony <libotony@users.noreply.github.com>
2021-12-08 08:14:58 +0000

    deploy: libotony/energy-station-web@31c78098d9c43297d6fb4b16a3c27768c44b6b65

---
+++ precache-manifest.d3f174507a10de294d74e17d4c88ae16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "eb0517cba63e30f601d3",
+    "url": "/js/chunk-vendors.eefe6fab.js"
+  },
+  {
+    "revision": "30c70b52ec06b11abe18",
+    "url": "/js/app.b1431119.js"
+  },
+  {
+    "revision": "c298f3714a00f87cab72926e80e2d0d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0517cba63e30f601d3",
+    "url": "/css/chunk-vendors.1e7c2657.css"
+  },
+  {
+    "revision": "30c70b52ec06b11abe18",
+    "url": "/css/app.c3308bc6.css"
+  },
+  {
+    "revision": "8087aeae0b3d6df5f9cbb96014bab133",
+    "url": "/404.html"
+  }
+];
